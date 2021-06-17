8f58fe52b5a725185e7c4074f60d216dc970c941
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-06-18 01:58:05 +0900

    一旦

--- pages/[uuid].tsx
+++ pages/[uuid].tsx
@@ -14,7 +14,10 @@ type Props = {
 
 const Result: NextPage<Props> = (props) => {
   const { uuid } = props;
-  const [ogp, setOgp] = useState("/muji.png");
+  // const [ogp, setOgp] = useState("/muji.png");
+  const [ogp, setOgp] = useState(
+    "https://firebasestorage.googleapis.com/v0/b/kudasaitter.appspot.com/o/wxwsj9fo?alt=media&token=adc28e7a-8c3a-4622-ad62-740f1a342d9e"
+  );
 
   useEffect(() => {
     const getUrl = async () => {
